e01680e133272ba475498a1a59216affa7b047ce
Denis <den1-1@mail.ru>
2018-01-27 21:25:58 +0300

    Added description for intent

--- src/application/intent.py
+++ src/application/intent.py
@@ -1,9 +1,10 @@
 class Intent:
-    def __init__(self, name_str, key_phrases_list, parameters_list, samples=None):
+    def __init__(self, name_str, key_phrases_list, parameters_list, samples=None, **kargs):
         self.__name = name_str
         self.__key_phrases_list = key_phrases_list
         self.__parameters_list = parameters_list
         self.__samples_list = samples
+        self.__description = kargs["description"]
 
     def get_name(self):
         return self.__name
@@ -16,3 +17,7 @@ class Intent:
 
     def get_samples(self):
         return self.__samples_list
+
+    @property
+    def description(self):
+        return self.__description
