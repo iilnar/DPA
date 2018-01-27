dacc5a545da152991f422d1cb11a76df89052bbd
Denis <den1-1@mail.ru>
2018-01-27 21:27:04 +0300

    1) Added welcome message 2) Added description for intents of ExplanationModule

--- src/moduls/self_introduction.py
+++ src/moduls/self_introduction.py
@@ -4,15 +4,30 @@ from answer import AssistantAnswer
 
 class SelfIntroductionModule:
     HEAD_PATTERN = "{} - {}"
-    INTENT_PATTERN = "   {}"
+    INTENT_PATTERN = "   -{}"
+    INTENT_WITH_DESC_PATTERN = "   -{}: {}"
 
     def run(self, assistant, parameters_dict):
+        intent = parameters_dict["Intent"]
+        answer = None
+        if intent == "Ability demonstration":
+            answer = self.ability_demonstration(assistant, parameters_dict)
+        elif intent == "Say hi":
+            answer = AssistantAnswer(mc.HI_MESSAGE)
+        return answer
+
+
+    def ability_demonstration(self, assistant, parameters_dict):
         apps = assistant.application_dict
         lines = []
         for app_name, app_desc in apps.items():
             lines.append(SelfIntroductionModule.HEAD_PATTERN.format(app_desc.get_name(), app_desc.get_description()))
             for intent in app_desc.get_intents_list():
-                line = SelfIntroductionModule.INTENT_PATTERN.format(intent.get_name())
+                desc = intent.description
+                if desc is None:
+                    line = SelfIntroductionModule.INTENT_PATTERN.format(intent.get_name())
+                else:
+                    line = SelfIntroductionModule.INTENT_WITH_DESC_PATTERN.format(intent.get_name(), desc)
                 lines.append(line)
         lines = "\n".join(lines)
         answer = AssistantAnswer(mc.INTRODUCTION_MESSAGE, parameters_dict={"desc": lines})
