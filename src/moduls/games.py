4507d750cd7a689767ad2e75b5c6017dab4335e8
Denis <den1-1@mail.ru>
2018-01-28 16:57:48 +0300

    Added win strategy for Matches game

--- src/moduls/games.py
+++ src/moduls/games.py
@@ -2,11 +2,20 @@ from answer import AssistantAnswer
 import random
 
 
+class MatchesBot:
+    def move(self, left):
+        for i in range(1, min(left, 3) + 1):
+            if (left - i - 1) % 4 == 0:
+                return i
+        return 1
+
+
 class MatchesGameModule:
 
     def __init__(self, config):
         self.amount_of_matches = 0
         self.is_started = False
+        self.bot = MatchesBot()
 
     def run(self, assistant, parameters_dict):
         intent = parameters_dict["Intent"]
@@ -17,9 +26,8 @@ class MatchesGameModule:
             answer = self.turn(assistant, parameters_dict)
         return answer
 
-
     def start(self, assistant, parameters_dict):
-        self.amount_of_matches = 30
+        self.amount_of_matches = int(parameters_dict.get("AmountOfMatches", 30))
         self.is_started = True
         return AssistantAnswer("matches_game.start_game", {"matches": self.amount_of_matches})
 
@@ -33,7 +41,7 @@ class MatchesGameModule:
             amount_to_remove = int(parameters_dict["Amount"])
             if 1 <= amount_to_remove <= 3:
                 self.amount_of_matches -= amount_to_remove
-                bot_choice = random.randint(1, 3)
+                bot_choice = self.bot.move(self.amount_of_matches)
                 if self.amount_of_matches < 2:
                     answer = AssistantAnswer("matches_game.win")
                     self.is_started = False
